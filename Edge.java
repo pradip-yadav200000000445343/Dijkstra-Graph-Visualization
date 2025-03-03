5614f0357559caed2d8bd29528c53fe4e6df7492
Pradip Yadav <pradiplyadav15092003@gmail.com>
2025-03-03 21:36:44 +0530

    Create Edge.java

---
+++ Edge.java
@@ -0,0 +1,9 @@
+class Edge {
+    int destination;
+    int weight;
+
+    public Edge(int destination, int weight) {
+        this.destination = destination;
+        this.weight = weight;
+    }
+}
